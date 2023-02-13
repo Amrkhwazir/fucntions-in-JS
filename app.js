db1875127088c317860a92bf31ec6c57ca19705e
mughees-khan <mugheesk180@gmail.com>
2023-02-13 09:47:06 +0500

    function practice

---
+++ app.js
@@ -0,0 +1,37 @@
+
+// with return keyword //
+function addition(a, b){
+    return a+b
+    console.log(a+b)
+    console.log(a+b)
+}
+
+console.log(addition(3,5))
+console.log(addition(3,8))
+
+// without return keyword //
+
+function subtraction(x, y){
+
+    console.log(x-y)
+    console.log(x-y)
+}
+subtraction(3,5)
+subtraction(3,8)
+
+// with prompt //
+var userName = prompt("enter your name")
+function name(variable1){
+    alert(variable1)
+}
+name(userName)
+
+// with prompt with return keyword //
+
+var userName = prompt("enter your name")
+function name(z){
+    return z
+    alert(variable1)
+}
+
+document.write(name(userName))
